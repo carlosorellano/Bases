3a4e17feb0b520cf217294d81e7893fec099a162
Carlos-Orellano <corellano02@gmail.com>
2025-09-18 23:11:04 -0500

    agregamos la pagina de dragonball

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CounterPageComponet } from './pages/counter/counter-page.component';
 
 import {HeroPageComponent} from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
 {
@@ -12,7 +13,18 @@ export const routes: Routes = [
 {
     path: 'hero',
     component: HeroPageComponent
-}
+},
+
+
+{
+    path: 'dragonball',
+    component: DragonballPageComponent
+},
+
+{
+    path: '**',
+    redirectTo:  '', // este path redirecciona hacia inicio 
+},
 
 ];
 
--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,19 +1,30 @@
+import { UpperCasePipe } from '@angular/common';
 import { App } from './../../app';
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
     selector: 'app-hero-page',
     templateUrl: './hero-page.component.html',
-      styleUrls: ['./hero-page.component.css']
+    imports: [UpperCasePipe],
+    styleUrls: ['./hero-page.component.css'],
+    
+
 })
 export class HeroPageComponent {
 
     name = signal('Ironman')
     age = signal(45)
 
-    getHeroDescription() {
-        return `${this.name()} - ${this.age()}`;
-    }
+    heroDescription = computed(() => {
+        const description = `${this.name()} - ${this.age()}`
+        return description
+    })
+
+    capitalizedName = computed(() => this.name().toUpperCase())
+
+    // getHeroDescription() {
+    //     return `${this.name()} - ${this.age()}`;
+    // }
     changeHero() {
         this.name.set('Spiderman')
         this.age.set(22)
